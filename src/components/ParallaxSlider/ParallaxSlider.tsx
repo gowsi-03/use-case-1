929384078c9e695bce2722645f20d3fb04739c0d
Prasath Soosaithasan <prasath.soosaithasan@external.contentful.com>
2025-05-10 12:17:38 +0530

    feat(ParallaxSlider): add boilerplate for ParallaxSlider component with TypeScript setup

---
+++ src/components/ParallaxSlider/ParallaxSlider.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ParallaxSlider = () => {
+  return (
+    <div>ParallaxSlider</div>
+  )
+}
+
+export default ParallaxSlider
---
+++ src/components/ParallaxSlider/index.ts
@@ -0,0 +1,1 @@
+export { default as ParallaxSlider } from './ParallaxSlider'
