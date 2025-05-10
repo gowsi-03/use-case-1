d2a3ace9b7a3368da5a917e253fbe1633090c5fe
Prasath Soosaithasan <prasath.soosaithasan@external.contentful.com>
2025-05-10 12:33:17 +0530

    feat(ParallaxSlider): implement compound component with Slide subcomponent

--- src/components/ParallaxSlider/ParallaxSlider.tsx
+++ src/components/ParallaxSlider/ParallaxSlider.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+'use client';
 
-const ParallaxSlider = () => {
-  return (
-    <div>ParallaxSlider</div>
-  )
+import Image from 'next/image';
+import React, { ReactNode } from 'react';
+
+interface SlideProps {
+  title: string;
+  subtitle: string;
+  image: string;
 }
 
-export default ParallaxSlider
+interface ParallaxSliderProps {
+  children: ReactNode;
+}
+
+const Slide: React.FC<SlideProps> = ({ title, subtitle, image }) => {
+  return (
+    <div className="slide">
+      <Image src={image} alt={title} width={200} height={200}/>
+      <h2>{title}</h2>
+      <p>{subtitle}</p>
+    </div>
+  );
+};
+
+const ParallaxSlider: React.FC<ParallaxSliderProps> & { Slide: React.FC<SlideProps> } = ({ children }) => {
+  return <div className="parallax-slider">{children}</div>;
+};
+
+// Attach Slide as a subcomponent
+ParallaxSlider.Slide = Slide;
+
+export default ParallaxSlider;
