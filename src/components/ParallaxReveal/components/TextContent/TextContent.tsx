e5205fa0a754321bfc88961968bb2b5ed43165ee
Prasath Soosaithasan <prasath.soosaithasan@external.contentful.com>
2025-05-12 09:30:22 +0530

    feat(TextContent): render <br> as line breaks in subtitles safely

--- src/components/ParallaxReveal/components/TextContent/TextContent.tsx
+++ src/components/ParallaxReveal/components/TextContent/TextContent.tsx
@@ -1,5 +1,6 @@
 import { Typography } from "antd";
 import styles from "./TextContent.module.css";
+import React from "react";
 
 const { Title, Paragraph } = Typography;
 
@@ -8,16 +9,28 @@ type Props = {
   subtitle: string;
 };
 
-const TextContent = ({ title, subtitle }: Props) => (
-  <div>
-    <div className={styles.sectionTitle}>
-      <Title level={3}>{title}</Title>
-    </div>
-    <div className={styles.sectionPara}>
-      <Paragraph>{subtitle}</Paragraph>
-    </div>
-  </div>
-);
+const TextContent = ({ title, subtitle }: Props) => {
+  const hasLineBreaks = /<br\s*\/?>/i.test(subtitle);
 
+  return (
+    <div className={styles.textContentWrapper}>
+      <div className={styles.sectionTitle}>
+        <Title level={3}>{title}</Title>
+      </div>
+      <div className={styles.sectionPara}>
+        <Paragraph>
+          {hasLineBreaks
+            ? subtitle.split(/<br\s*\/?>/i).map((part, idx, arr) => (
+                <React.Fragment key={idx}>
+                  {part.trim()}
+                  {idx < arr.length - 1 && <br />}
+                </React.Fragment>
+              ))
+            : subtitle}
+        </Paragraph>
+      </div>
+    </div>
+  );
+};
 
-export default TextContent
+export default TextContent;
